3cb8e0cfd7fdb453304bb2354337125f054505e5
TierryBr <tierrybarros15@gmail.com>
2021-09-03 11:48:08 -0300

    criação da sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Routes from './routes';
+import Sidebar from './Components/Sidebar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes />
     </BrowserRouter>
   );
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+import { FiShoppingBag } from 'react-icons/fi';
+import "../styles/sidebar.scss";
+ 
+import logo from '../assets/m.png';
+ 
+export default function Header() {;
+ return (
+   <header className="header">
+     <Link to="/" className="logo">
+       <img className="logo-icon" src={logo} alt="Marvel" />
+       <span className="logo-text">arvel</span>
+     </Link>
+ 
+     <Link to="/cart" className="header-cart">
+       <div>
+         <strong>Carrinho</strong>
+         <span>
+           <strong>4</strong> quadrinhos
+         </span>
+       </div>
+       <FiShoppingBag size={36} color="#1C1C1C" />
+     </Link>
+   </header>
+ );
+}
